79321bb894e23ef08039b9a747d60976099f7160
kyleochata <kyleochata@gmail.com>
2023-10-23 18:28:12 -0700

    folder structure added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 
+import App from './App.jsx'
+import ErrorPage from './pages/ErrorPage/ErrorPage'
+import HomePage from './pages/HomePage/HomePage'
+import AboutPage from './pages/AboutPage/AboutPage'
+import ProjectsPage from './pages/ProjectsPage/ProjectsPage'
+import ContactPage from './pages/ContactPage/ContactPage'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: '/about',
+        element: <AboutPage />,
+      },
+      {
+        path: '/contact',
+        element: <ContactPage />,
+      },
+      {
+        path: '/projects',
+        element: <ProjectsPage />,
+      },
+    ],
+  },
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
