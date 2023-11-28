e8be87e05c49259fb6ae9beec626c1224c1fbac4
kyleochata <kyleochata@gmail.com>
2023-11-27 20:31:18 -0800

    contact page done

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -10,10 +10,52 @@ const ContactPage = () => {
         <Header />
         <section className={style.ContactSection}>
           <h1 className={style.ContactTitle}>Contact Me</h1>
+          <p className={style.ContactText}>
+            If would like to get in touch for an opportunity or have questions,
+            please feel free to reach out to me with the information below.
+          </p>
           <div className={style.ContactContainer}>
-            <div className={style.ContactEmail}>kyleochata@gmail.com</div>
-            <div className={style.ContactPhone}>1-714-917-9092</div>
-            <div className={style.ContactSocial}>
+            <div className={style.CCContainer}>
+              <div>Email:</div>
+              <div>
+                <a
+                  href="mailto:kyleochata@gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  kyleochata@gmail.com
+                </a>
+              </div>
+            </div>
+            <div className={style.CCContainer}>
+              <div>Phone:</div>
+              <div>1-714-917-9092</div>
+            </div>
+            <div className={style.CLContainer}>
+              <div>LinkedIn</div>
+              <div>
+                <a
+                  href="https://www.linkedin.com/in/kyle-etrata"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  linkedin.com/in/kyle-etrata
+                </a>
+              </div>
+            </div>
+            <div className={style.CLContainer}>
+              <div>GitHub</div>
+              <div>
+                <a
+                  href="https://github.com/kyleochata"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  github.com/kyleochata
+                </a>
+              </div>
+            </div>
+            {/* <div className={style.ContactSocial}>
               <div className={style.ContactLinks}>
                 Linked:
                 <a
@@ -34,7 +76,7 @@ const ContactPage = () => {
                   www.github.com/kyleochata
                 </a>
               </div>
-            </div>
+            </div> */}
           </div>
         </section>
         <Footer />
